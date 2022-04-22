2e17f7bec418097c5db00433608799db6d8acaa3
Prateek Shukla <prateek.shukla@iauro.com>
2022-04-22 18:26:04 +0530

    reorganizing the files into Expense related files and General UI related files as subfolders inside components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 function ExpenseItem(props) {
   const { title, amount, date } = props;
