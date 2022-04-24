3a9a7fb289978d3a057d174f4eb6686b9d188cfd
Prateek Shukla <prateek.shukla@iauro.com>
2022-04-24 15:51:29 +0530

    useState react hook for changing the state of title on click of the test update button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
@@ -7,13 +7,21 @@ import Card from "../UI/Card";
 const ExpenseItem = (props) => {
   const { title, amount, date } = props;
 
+  const [currentTitle, setCurrentTitle] = useState(title);
+
+  const clickHandler = () => {
+    setCurrentTitle('Updated title');
+    console.log(currentTitle)
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{currentTitle}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={clickHandler}>Update the title</button>
     </Card>
   );
 }
